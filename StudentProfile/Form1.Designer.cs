cd89755668682c12845019a0cce44cf5297753af
billonesjoannekristel355-code <billonesjoannekristel355@gmail.com>
2026-09-13 18:19:45 +0800

    add student contact number

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(70, 68);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(225, 16);
+            this.label1.Size = new System.Drawing.Size(179, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Student Profile: Github- Beginner-Lab";
+            this.label1.Text = "Contact Number: 0917234567";
             // 
             // Form1
             // 
